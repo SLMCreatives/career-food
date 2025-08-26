a494d252b67b55e0399661c716162138a7949bd7
SLMCreatives <sulaiman@slmcreatives.com>
2025-08-26 11:30:23 +0800

    add campus in upload file

--- career-food/app/sgs/upload/page.tsx
+++ career-food/app/sgs/upload/page.tsx
@@ -81,7 +81,7 @@ export default function HomePage() {
     <div className="min-h-screen items-center justify-center bg-gray-100 p-4 absolute inset-0 flex">
       <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-lg">
         <h1 className="mb-6 text-center text-3xl font-bold text-gray-800">
-          Update SGS Leaderboard
+          Update SGS Leaderboard with campus info
         </h1>
 
         <UploadForm />
